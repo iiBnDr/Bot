06ae56927e53f809e2d316ce2b2d1cb90ab58383
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2018-12-13 20:39:52 +0300

    Add files via upload

---
+++ TyBC.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('@bc')) {
+          if (!args[0]) {
+message.channel.send("**@bc <message>**");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
+ 
+client.login('NTIyMzg0MjAyODE4MjU2ODk3.DvQoGg.EsJ27PxWXAD1psqsEkAy_JIVKDk');
